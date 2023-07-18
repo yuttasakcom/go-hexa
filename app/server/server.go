bc33aeec79888c6bba017cdce83c7bcb0030ac7b
Yuttasak(Yo)Pannawat <yuttasak.pan.true@gmail.com>
2023-07-18 22:24:25 +0700

    feat(server) Graceful shutdown

--- app/server/server.go
+++ app/server/server.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+
 	"github.com/yuttasakcom/go-hexa/app/config"
 	"github.com/yuttasakcom/go-hexa/app/database"
 	"github.com/yuttasakcom/go-hexa/app/router"
@@ -24,6 +28,14 @@ func NewServer(cfg config.IConfig, db *database.Store) IServer {
 
 func (s *Server) Start() {
 	app := router.NewApp()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		_ = <-c
+		fmt.Println("Gracefully shutting down...")
+		_ = app.Shutdown()
+	}()
+
 	router.Register(app, s.db)
 	app.Listen(s.cfg.App().Host())
 }
